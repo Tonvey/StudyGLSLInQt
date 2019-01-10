45765576e22a60fe30ca486c5c6872fa1c844457
liangboyan <liangboyanzaz@163.com>
2019-01-10 18:32:43 +0800

    modified indices

--- red_triangle_ebo/redtriangle_ebo.cpp
+++ red_triangle_ebo/redtriangle_ebo.cpp
@@ -13,18 +13,20 @@ RedTriangleEBO::RedTriangleEBO()
 {
     this->resize(600,400);
 }
-
 void RedTriangleEBO::initializeGL()
 {
     //vertexes data
     const float vertexes[] = {
-        -1.0f,-1.0f,0.0f,
-        1.0f,-1.0f,0.0f,
-        0.0f,1.0f,0.0f
+        0.5f, 0.5f, -1.0f,   // 右上角
+        0.5f, -0.5f, -1.0f,  // 右下角
+        -0.5f, -0.5f, -1.0f, // 左下角
+        -0.5f, 0.5f, -1.0f   // 左上角
     };
     //indices data
     const GLuint indices[] = {
-        0,1,2
+        // 起始于0!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
     };
     GLWindow::initializeGL();
     this->mShaderProgram = compileShader(":/red_triangle_ebo/shader.vert",":/red_triangle_ebo/shader.frag");
@@ -56,7 +58,6 @@ void RedTriangleEBO::initializeGL()
     mVAO.release();
     glClearColor(0,0,0,1);
 }
-
 void RedTriangleEBO::paintGL()
 {
     glClearColor(0, 0, 0, 1);
@@ -66,14 +67,13 @@ void RedTriangleEBO::paintGL()
     this->mVAO.bind();
     this->mVBO.bind();
     this->mEBO.bind();
-    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_INT,(void*)0);
+    glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,(void*)0);
     this->mEBO.release();
     this->mVBO.release();
     this->mVAO.release();
     this->mShaderProgram->disableAttributeArray(this->mVertexPositionName);
     this->mShaderProgram->release();
 }
-
 RedTriangleEBO::~RedTriangleEBO()
 {
     //TODO : Need destroy manually?
@@ -84,7 +84,3 @@ RedTriangleEBO::~RedTriangleEBO()
     if(mVAO.isCreated())
         mVAO.destroy();
 }
-
-
-
-
